0f6b195936b5f546a06d23267f33b561873b1a01
s2120651Jimenea <100011708+s2120651Jimenea@users.noreply.github.com>
2022-02-19 14:10:53 +0800

    Update index.php

--- index.php
+++ index.php
@@ -6,5 +6,7 @@
     <body>
     <h1>Welcome to My Website! I am Jia Michelle Jimenea!</h1>
     <ul>
+		<li><a href="recipes.html"> HOME </a> </li>
+		<li><a href="recipes.html"> Learning Task 2 </a></li>
     </body>
 </html>
