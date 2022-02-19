8df7751d8863195c7bd9b07d42b9ee03e0b9bc41
s2120651Jimenea <100011708+s2120651Jimenea@users.noreply.github.com>
2022-02-19 13:08:38 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>My Home Page</title>
+    </head>
+    <body>
+    <h1>Welcome to My Website! I am Jia Michelle Jimenea!</h1>
+    <ul>
+    </body>
+</html>
