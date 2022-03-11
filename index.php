da6c86cd5426b003116c195598542bbd5d7f64f7
s2120651Jimenea <100011708+s2120651Jimenea@users.noreply.github.com>
2022-03-11 22:10:28 +0800

    Update index.php

--- index.php
+++ index.php
@@ -4,9 +4,10 @@
         <title>My Home Page</title>
     </head>
     <body>
-    <h1>Welcome to My Website! I am Jia Michelle Jimenea!</h1>
+    <h1>Hi! Welcome to My Website, I am Jia Michelle Jimenea :)♡</h1>
     <ul>
 		<li><a href="index.php"> HOME </a> </li>
 		<li><a href="LT2/recipes.html"> Learning Task 2 </a></li>
+	    	<li><a href="LT3/index.html"> Learning Task 3 </a></li>
     </body>
 </html>
