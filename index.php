1bc32c72aa4c276e7b8cfe9ffbc8bb8d5c63aaa4
s2120651Jimenea <100011708+s2120651Jimenea@users.noreply.github.com>
2022-03-23 22:15:20 +0800

    Update index.php

--- index.php
+++ index.php
@@ -9,6 +9,6 @@
 		<li><a href="index.php"> HOME </a> </li>
 		<li><a href="LT2/recipes.html"> Learning Task 2 </a></li>
 	    	<li><a href="LT3/index.html"> Learning Task 3 </a></li>
-	    	<li><a href="CLE1MT/index.html"> Learning Task 3 </a></li>
+	    	<li><a href="CLE1MT/index.html"> CLE1: Culminating Learning Evidence (Midterm) </a></li>
     </body>
 </html>
