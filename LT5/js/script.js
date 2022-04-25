f22fd05ee194f8cecde11d8ece6db5ec84e21af3
s2120651Jimenea <100011708+s2120651Jimenea@users.noreply.github.com>
2022-04-25 21:26:22 +0800

    Add files via upload

---
+++ LT5/js/script.js
@@ -0,0 +1,28 @@
+var name = prompt("What is your name?");
+var month = prompt("What is your birth month?");
+	if (month === "January") {
+	document.body.innerHTML = "<h6>Hi " + name + "! " + month + " is your Birth Month so your Birth Month flower is Carnation!</h6>"
+	} else if (month === "February"){
+	document.body.innerHTML = "<h6>Hi " + name + "! " + month + " is your Birth Month so your Birth Month flower is Primrose!</h6>"
+	} else if (month === "March"){
+	document.body.innerHTML = "<h6>Hi " + name + "! " + month + " is your Birth Month so your Birth Month flower is Daffodil!</h6>"
+	} else if (month === "April"){
+	document.body.innerHTML = "<h6>Hi " + name + "! " + month + " is your Birth Month so your Birth Month flower is Daisy!</h6>"
+	} else if (month === "May"){
+	document.body.innerHTML = "<h6>Hi " + name + "! " + month + " is your Birth Month so your Birth Month flower is Lily of the Valley!</h6>"
+	} else if (month === "June"){
+	document.body.innerHTML = "<h6>Hi " + name + "! " + month + " is your Birth Month so your Birth Month flower is Rose!</h6>"
+	} else if (month === "July"){
+	document.body.innerHTML = "<h6>Hi " + name + "! " + month + " is your Birth Month so your Birth Month flower is Water Lily!</h6>"
+	} else if (month === "August"){
+	document.body.innerHTML = "<h6>Hi " + name + "! " + month + " is your Birth Month so your Birth Month flower is Poppy!</h6>"
+	} else if (month === "September"){
+	document.body.innerHTML = "<h6>Hi " + name + "! " + month + " is your Birth Month so your Birth Month flower is Aster!</h6>"
+	} else if (month === "October"){
+	document.body.innerHTML = "<h6>Hi " + name + "! " + month + " is your Birth Month so your Birth Month flower is Marigold!</h6>"
+	} else if (month === "November"){
+	document.body.innerHTML = "<h6>Hi " + name + "! " + month + " is your Birth Month so your Birth Month flower is Chrysanthemum!</h6>"
+	} else if (month === "December"){
+	document.body.innerHTML = "<h6>Hi " + name + "! " + month + " is your Birth Month so your Birth Month flower is Holly!</h6>"
+	} else{}
+alert("Explore the website to find out what is your birth flower and its meaning");
